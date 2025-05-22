0b6be661364e3a707301f056a52925b6bbcca6e6
Lee, Jeong Han <jeonghan.lee@gmail.com>
2025-05-22 02:59:08 -0700

    fixed the hardcode rpath with LD_LIBRARY_PATH solution

--- 1.1.1/debian-12/7.0.7/modules/autosave-10a8f11/include/autosave_release.h
+++ 1.1.1/debian-12/7.0.7/modules/autosave-10a8f11/include/autosave_release.h
@@ -1,1 +1,1 @@
-#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Wed May 14 12:40:38 2025"
+#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Thu May 22 02:50:46 2025"
--- 1.1.1/debian-12/7.0.7/modules/seq-2.2.9/include/seq_release.h
+++ 1.1.1/debian-12/7.0.7/modules/seq-2.2.9/include/seq_release.h
@@ -1,2 +1,2 @@
-#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Wed May 14 12:39:40 2025"
+#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Thu May 22 02:49:41 2025"
 #define MAGIC 2002009
