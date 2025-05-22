10a9bcd31ac7862b3a31b6cb0cb68bbbedc8c6b8
Jeong Han Lee <jeonghan.lee@gmail.com>
2025-05-22 08:18:16 -0700

    update ubuntu24

--- 1.1.1/ubuntu-24.04/7.0.7/modules/autosave-10a8f11/include/autosave_release.h
+++ 1.1.1/ubuntu-24.04/7.0.7/modules/autosave-10a8f11/include/autosave_release.h
@@ -1,1 +1,1 @@
-#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Thu May 15 00:04:21 2025"
+#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Thu May 22 03:36:00 2025"
--- 1.1.1/ubuntu-24.04/7.0.7/modules/seq-2.2.9/include/seq_release.h
+++ 1.1.1/ubuntu-24.04/7.0.7/modules/seq-2.2.9/include/seq_release.h
@@ -1,2 +1,2 @@
-#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Thu May 15 00:03:06 2025"
+#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Thu May 22 03:34:30 2025"
 #define MAGIC 2002009
