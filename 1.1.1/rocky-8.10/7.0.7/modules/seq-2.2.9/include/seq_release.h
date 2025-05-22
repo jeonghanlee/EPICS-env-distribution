e3cc3d746a530dc3cacd4fcb2d6d66f7416d68ad
Lee, Jeong Han <jeonghan.lee@gmail.com>
2025-05-22 06:26:28 -0400

    update rocky 8.10

--- 1.1.1/rocky-8.10/7.0.7/modules/seq-2.2.9/include/seq_release.h
+++ 1.1.1/rocky-8.10/7.0.7/modules/seq-2.2.9/include/seq_release.h
@@ -1,2 +1,2 @@
-#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Wed May 14 23:31:55 2025"
+#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Thu May 22 06:10:46 2025"
 #define MAGIC 2002009
