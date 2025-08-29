a18ae7179fb5a721f1c4b80a57a7e5e71411bddd
Lee, Jeong Han <jeonghan.lee@gmail.com>
2025-08-28 19:02:01 -0700

    remove all, update install.bash for 1.1.2 release

--- 1.1.1/rocky-9.6/7.0.7/modules/autosave-10a8f11/include/autosave_release.h
+++
@@ -1,1 +0,0 @@
-#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Thu Jun 26 03:43:02 2025"
--- 1.1.1/ubuntu-24.04/7.0.7/modules/autosave-10a8f11/include/autosave_release.h
+++
@@ -1,1 +0,0 @@
-#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Thu May 22 03:36:00 2025"
