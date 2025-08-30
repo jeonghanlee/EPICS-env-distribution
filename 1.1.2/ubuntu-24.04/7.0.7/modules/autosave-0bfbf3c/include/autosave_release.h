44e2b17e7524e99d849a6dd5734b20866e78a7fb
Jeong Han Lee <jeonghan.lee@gmail.com>
2025-08-30 12:57:16 -0700

    add ubuntu 24

---
+++ 1.1.2/ubuntu-24.04/7.0.7/modules/autosave-0bfbf3c/include/autosave_release.h
@@ -0,0 +1,1 @@
+#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Sat Aug 30 12:45:33 2025"
---
+++ 1.1.2/ubuntu-24.04/7.0.7/modules/seq-2.2.9/include/seq_release.h
@@ -0,0 +1,2 @@
+#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Sat Aug 30 12:44:17 2025"
+#define MAGIC 2002009
