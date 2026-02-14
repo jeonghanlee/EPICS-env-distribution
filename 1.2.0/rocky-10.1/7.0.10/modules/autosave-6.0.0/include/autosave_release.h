bda71307c78b3af02b5de50dc6b53b9b7bd80c1c
Lee, Jeong Han <jeonghan.lee@gmail.com>
2026-02-14 02:40:51 -0800

    add rocky 10 to 1.2.0

---
+++ 1.2.0/rocky-10.1/7.0.10/modules/autosave-6.0.0/include/autosave_release.h
@@ -0,0 +1,1 @@
+#define AUTOSAVE_RELEASE "Autosave release 5.10.3, compiled Sat Feb 14 00:46:25 2026"
---
+++ 1.2.0/rocky-10.1/7.0.10/modules/seq-2.2.9/include/seq_release.h
@@ -0,0 +1,2 @@
+#define SEQ_RELEASE "Sequencer release 2.2.9, compiled Sat Feb 14 00:45:00 2026"
+#define MAGIC 2002009
